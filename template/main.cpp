55c6d5b02332ddb31d8bb0cf2831ce3469df7b91
Ilya Shpigor <petrsum@gmail.com>
2019-11-24 18:00:48 +0100

    Add the template example

---
+++ template/main.cpp
@@ -0,0 +1,9 @@
+import math;
+
+#include <cstdio>
+
+int main()
+{
+    printf("square = %d\n", square(42));
+    return 0;
+}
---
+++ template/math.cppm
@@ -0,0 +1,4 @@
+export module math;
+
+export template <typename T>
+T square(T a) { return a * a; }
